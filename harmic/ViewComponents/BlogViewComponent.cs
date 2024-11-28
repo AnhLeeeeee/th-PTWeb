e5a184999f26da6c11bc357637268dea6be7bd30
AnhLeeeeee <levananh13032k4@gmail.com>
2024-11-28 20:28:33 +0700

    th6_web

--- harmic/ViewComponents/BlogViewComponent.cs
+++ harmic/ViewComponents/BlogViewComponent.cs
@@ -10,15 +10,15 @@ namespace harmic.ViewComponents
         private readonly Harmic1Context _context;
 
 
-        public BlogViewComponent(Harmic1Context context)
+        public BlogViewComponent(Harmic1Context  context)
         {
 
-            _context = context;
+            _context =  context;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult>  InvokeAsync()
         {
-            var items = _context.TbBlogs.Include(m => m.Category)
+            var items = _context.TbBlogs.Include (m => m.Category)
                 .Where(m => (bool)m.IsActive);
             return await Task.FromResult<IViewComponentResult>
             (View(items.OrderByDescending(m => m.BlogId).ToList()));
