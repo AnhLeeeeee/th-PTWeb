fc8382c041d1393dedad4bac3c91542c9b3de737
AnhLeeeeee <levananh13032k4@gmail.com>
2024-12-03 09:51:42 +0700

    th7

---
+++ harmic/Areas/Admin/Controllers/MenusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace harmic.Areas.Admin.Controllers
+{
+    public class MenusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ harmic/Utilities/Function.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace harmic.Utilities
+{
+    public class Function : Controller
+    {
+        public static string TitleSlugGenerationAlias(string title)
+        {
+            return SlugGenerator.SlugGenerator.GenerateSlug(title);
+        }
+    }
+}
